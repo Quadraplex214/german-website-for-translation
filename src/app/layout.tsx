d945c0f4927e552bc9e9098df97a168c4c80f649
QACAMB <joshua.almeida@camb.ai>
2025-09-21 19:23:01 +0400

    For taking SS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,8 +44,8 @@ export default function RootLayout({
           </ThemeProvider>
         </LanguageProvider>
         <Script
-          src="https://storage.googleapis.com/website-translation-script/translator.dev.js"
-          data-api-key="wt_aa9552cfdce840cc_6HvF_pTTZuY8MZGj5gvDwQ"
+          src="https://storage.googleapis.com/website-translation-script/translator.staging.js"
+          data-api-key="wt_2c121c6f8f8a48e1_fVa-wZ8814aG-CJ3ge5u3Q"
         />
       </body>
     </html>
