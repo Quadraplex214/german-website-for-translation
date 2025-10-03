c52e2e63ac04324c2c7e2f0fb7c8c945d983cd2e
Quadraplex214 <josh.joshua.almeida@gmail.com>
2025-10-03 15:51:06 +0400

    Back to a dev key

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,8 +44,8 @@ export default function RootLayout({
           </ThemeProvider>
         </LanguageProvider>
         <Script
-          src="https://storage.googleapis.com/website-translation-script/translator.js"
-          data-api-key="wt_6b272245b7264ffd_bceY03d9_-p4BoiMsNF52A"
+          src="https://storage.googleapis.com/website-translation-script/translator.dev.js"
+          data-api-key="wt_d978214025b6426c_6kdMJV3IYlvQETBh1ymdlg"
         />
       </body>
     </html>
