28b88e9315d1dc867338d963e4a1e94c5a0149a1
Quadraplex214 <josh.joshua.almeida@gmail.com>
2025-10-08 12:03:00 +0400

    Removing Website Translation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,10 +43,6 @@ export default function RootLayout({
             <Toaster />
           </ThemeProvider>
         </LanguageProvider>
-        <Script
-          src="https://storage.googleapis.com/website-translation-script/translator.dev.js"
-          data-api-key="wt_b9ed8167461c4295_zPUY-o__Npqvgakacn13sA"
-        />
       </body>
     </html>
   );
