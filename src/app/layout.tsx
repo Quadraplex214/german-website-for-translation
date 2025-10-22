5275371ca5a15c95344b12fc820d07d1983c7905
Quadraplex214 <josh.joshua.almeida@gmail.com>
2025-10-22 22:23:49 +0400

    Let's see if Vibecoding works :/ Part 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
         <Script
           src="/website-translation/websiteTranslate.js"
           data-api-key="wt_b9ed8167461c4295_zPUY-o__Npqvgakacn13sA"
-          data-toggle="yes"
+          data-camb-toggle="yes"
         />
       </body>
     </html>
