6bf0d8053717c006de977b4cb10a2c262ec1efea
Quadraplex214 <josh.joshua.almeida@gmail.com>
2026-02-09 21:17:57 +0400

    Dev Testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
         <Script
           src="https://storage.googleapis.com/website-translation-script/translator.dev.js"
           data-api-key="wt_b9ed8167461c4295_zPUY-o__Npqvgakacn13sA"
-          data-disable-auto-browser-translation="true"
+          // data-disable-auto-browser-translation="true"
         />
       </body>
     </html>
