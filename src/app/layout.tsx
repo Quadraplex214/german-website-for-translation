bb00b5432aa22a2c3af38e4ce8c3c12299ef6389
Quadraplex214 <josh.joshua.almeida@gmail.com>
2026-03-04 07:21:15 +0400

    Back to dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
           </ThemeProvider>
         </LanguageProvider>
         <Script
-          src="website-translation/websiteTranslate.js"
+          src="https://storage.googleapis.com/website-translation-script/translator.dev.js"
           data-api-key={process.env.NEXT_PUBLIC_TRANSLATOR_API_KEY || "wt_b9ed8167461c4295_zPUY-o__Npqvgakacn13sA"}
           data-disable-auto-browser-translation="true"
         />
